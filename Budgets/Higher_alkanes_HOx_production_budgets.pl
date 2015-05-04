3ee267f9cf4d80fb3a9856971ddc8c992d7f572f
ganna10 <janecoates10@gmail.com>
2015-05-04 16:21:07 +0200

    Cumulative HOx and Ox budgets

--- Budgets/Higher_alkanes_HOx_production_budgets.pl
+++ Budgets/Higher_alkanes_HOx_production_budgets.pl
@@ -1,7 +1,7 @@
 #! /usr/bin/env perl
 # Compare HOx production budgets from the higher alkanes between speciations and mechanisms, Solvent only runs
 # Version 0: Jane Coates 29/4/2015
-#
+
 use strict;
 use diagnostics;
 use KPP;
@@ -12,6 +12,7 @@ use Statistics::R;
 
 #my $base = "/work/users/jco/Solvent_Emissions";
 my $base = "/local/home/coates/Solvent_Emissions";
+#my @mechanisms = qw( RADM2 );
 my @mechanisms = qw( MCM MOZART RADM2 );
 #my @speciations = qw( GR95 );
 my @speciations = qw( TNO IPCC DE94 GR95 GR05 UK98 UK08 );
@@ -82,11 +83,24 @@ foreach my $mechanism (sort keys %data) {
 #print $p, "\n";
 
 $R->run(q` data$Speciation = factor(data$Speciation, levels = c("TNO", "IPCC", "DE94", "GR95", "GR05", "UK98", "UK08")) `);
-$R->run(q` plot = ggplot(data, aes(x = Mechanism, y = HOx, fill = Species)) `,
+$R->run(q` data$Species = factor(data$Species, levels = c("NC6H14", "M2PE", "M3PE", "CHEX", "NC7H16", "M2HEX", "M3HEX", "NC8H18", "NC9H20", "NC10H22", "NC11H24", "NC12H26")) `);
+$R->run(q` my.colours = c("NC6H14" = "#6c254f", "M2PE" = "#f9c500", "M3PE" = "#0e5c28", "CHEX" = "#ef6638", "NC7H16" = "#2b9eb3", "M2HEX" = "#b569b3", "M3HEX" = "#f7c56c", "NC8H18" = "#0352cb", "NC9H20" = "#ae4901", "NC10H22" = "#4c9383", "NC11H24" = "#8c1531", "NC12H26" = "#77aecc") `); 
+
+$R->run(q` plot = ggplot(data, aes(x = Mechanism, y = HOx, fill = Species, order = Species)) `,
         q` plot = plot + geom_bar(stat = "identity") `,
         q` plot = plot + facet_wrap( ~ Speciation, nrow = 2) `,
         q` plot = plot + theme_tufte() `,
         q` plot = plot + theme(axis.line = element_line(colour = "black")) `,
+        q` plot = plot + theme(strip.text = element_text(face = "bold")) `,
+        q` plot = plot + theme(axis.title.x = element_blank()) `,
+        q` plot = plot + theme(legend.title = element_blank()) `,
+        q` plot = plot + theme(axis.text.x = element_text(face = "bold")) `,
+        q` plot = plot + theme(axis.title.y = element_text(face = "bold")) `,
+        q` plot = plot + theme(panel.margin = unit(5, "mm")) `,
+        q` plot = plot + ylab("Total HOx Production (molecules cm-3)") `,
+        q` plot = plot + scale_y_continuous(expand = c(0, 0)) `,
+        q` plot = plot + scale_x_discrete(expand = c(0, 0)) `,
+        q` plot = plot + scale_fill_manual(values = my.colours, limits = rev(levels(data$Species))) `,
 );
 
 $R->run(q` CairoPDF(file = "Higher_alkanes_HOx_production_facet_speciation.pdf", width = 10, height = 7) `,
@@ -135,27 +149,50 @@ sub get_data {
         $consumption_rates{$parent} += $rate(1:$ntime-2);
     }
     remove_common_processes(\%production_rates, \%consumption_rates);
+    $production_rates{$_} = $production_rates{$_}->sum foreach (keys %production_rates);
     
+    #print "before\n";
+    #print "$_ : $production_rates{$_}\n" foreach keys %production_rates;
     my %contributions;
-    if ($mechanism eq "RADM2" or $mechanism eq "MOZART") {
-        open my $in, '<:encoding(utf-8)', "Higher_alkanes_RADM2_MOZ_fractional_contributions.csv" or die $!;
+    if ($mechanism eq "RADM2" or $mechanism eq "MOZART") { #allocation of RADM2 and MOZART emitted species to MCM species that are Higher Alkanes
+        open my $in, '<:encoding(utf-8)', "Higher_alkanes_contributions_of_MCM_species_in_MOZART_RADM2.csv" or die $!;
         my @lines = <$in>;
         close $in;
         foreach my $line (@lines) {
             chomp $line;
-            my ($mechanism, $speciation, $species, $contribution) = split /,/, $line;
-            $contributions{$mechanism}{$speciation}{$species} = $contribution;
+            my ($mechanism, $speciation, $mech_species, $mcm_species, $contribution) = split /,/, $line;
+            $contributions{$mechanism}{$speciation}{$mech_species}{$mcm_species} = $contribution;
         }
 
         foreach my $species (keys %production_rates) {
             if (defined $contributions{$mechanism}{$speciation}{$species}) {
-                $production_rates{$species} *= $contributions{$mechanism}{$speciation}{$species};
-            } else {
-                delete $production_rates{$species};
-            } 
+                foreach my $mcm_species (keys %{$contributions{$mechanism}{$speciation}{$species}}) {
+                    $production_rates{$mcm_species} = $contributions{$mechanism}{$speciation}{$species}{$mcm_species} * $production_rates{$species};
+                }
+            }
+            delete $production_rates{$species};
         }
     }
-    $production_rates{$_} = $production_rates{$_}->sum foreach (keys %production_rates);
+    #print "after\n";
+    #print "$_ : $production_rates{$_}\n" foreach keys %production_rates;
+#    if ($mechanism eq "RADM2" or $mechanism eq "MOZART") { #allocation of RADM2 and MOZART emitted species to Higher Alkanes
+#        open my $in, '<:encoding(utf-8)', "Higher_alkanes_RADM2_MOZ_fractional_contributions.csv" or die $!;
+#        my @lines = <$in>;
+#        close $in;
+#        foreach my $line (@lines) {
+#            chomp $line;
+#            my ($mechanism, $speciation, $species, $contribution) = split /,/, $line;
+#            $contributions{$mechanism}{$speciation}{$species} = $contribution;
+#        }
+#
+#        foreach my $species (keys %production_rates) {
+#            if (defined $contributions{$mechanism}{$speciation}{$species}) {
+#                $production_rates{$species} *= $contributions{$mechanism}{$speciation}{$species};
+#            } else {
+#                delete $production_rates{$species};
+#            } 
+#        }
+#    }
     return \%production_rates;
 }
 
