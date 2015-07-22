1d3af50a93dec3c88d6d3617153ffe8060952d75
ganna10 <janecoates10@gmail.com>
2015-07-22 16:30:42 +0200

    Correlation plots facetted by type and mechanism

---
+++ Correlations/Correlate_Ox_to_alkanes_aromatics_oxygenated_emissions_first_day.pl
@@ -0,0 +1,268 @@
+#! /usr/bin/env perl
+# Correlate fraction of alkanes, aromatics and oxygenated of VOC emissions to first day Ox production in each mechanism
+# Version 0: Jane Coates 22/7/2015
+
+use strict;
+use diagnostics;
+use KPP;
+use MECCA;
+use PDL;
+use PDL::NiceSlice;
+use Statistics::R;
+
+#my $base = "/work/users/jco/Solvent_Emissions";
+my $base = "/local/home/coates/Solvent_Emissions";
+my @mechanisms = qw( MOZART RADM2 );
+#my @mechanisms = qw( MCM MOZART RADM2 );
+#my @speciations = qw( EMEP UK08 );
+my @speciations = qw( TNO IPCC EMEP DE94 GR95 GR05 UK98 UK08 );
+my (%families, %weights, %Ox_production, %emissions);
+
+my $mecca = MECCA->new("$base/RADM2/DE94_Solvents_Only/boxmodel");
+my $ntime = $mecca->time->nelem;
+my $dt = $mecca->dt->at(0);
+my $n_per_day = 86400 / $dt;
+my $n_days = int $ntime / $n_per_day;
+
+foreach my $mechanism (@mechanisms) {
+    foreach my $speciation (@speciations) {
+        my $dir = "$base/$mechanism/${speciation}_Solvents_Only";
+        my $mecca = MECCA->new("$dir/boxmodel");
+        my $kpp = KPP->new("$dir/gas.eqn");
+        my $ro2_file = "$dir/RO2_species.txt";
+        my @no2_reservoirs = get_no2_reservoirs($kpp, $ro2_file);
+        $families{"Ox"} = [ qw( O3 O O1D NO2 NO3 N2O5 HO2NO2 ), @no2_reservoirs ];
+        $weights{"Ox"} = { NO3 => 2, N2O5 => 3 };
+        $Ox_production{$mechanism}{$speciation} = get_Ox_production($kpp, $mecca);
+        $emissions{$mechanism}{$speciation} = get_emission_fractions($kpp, $mecca, $mechanism, $speciation);
+    }
+}
+
+my $R = Statistics::R->new();
+$R->run(q` library(ggplot2) `,
+        q` library(Cairo) `,
+        q` library(tidyr) `,
+        q` library(dplyr) `,
+        q` library(scales) `,
+        q` library(grid) `,
+        q` library(ggthemes) `,
+        q` library(gridExtra) `,
+        q` library(gtable) `,
+);
+$R->run(q` data = data.frame() `);
+foreach my $mechanism (sort keys %emissions) {
+    $R->set('mechanism', $mechanism);
+    foreach my $speciation (sort keys %{$emissions{$mechanism}}) {
+        $R->set('speciation', $speciation);
+        $R->set('ox', $Ox_production{$mechanism}{$speciation});
+        $R->run(q` pre = data.frame(Mechanisms = mechanism, Speciation = speciation, Ox = ox) `);
+        foreach my $type (sort keys %{$emissions{$mechanism}{$speciation}}) {
+            $R->set('type', $type);
+            $R->set('emission.fraction', $emissions{$mechanism}{$speciation}{$type});
+            $R->run(q` pre[type] = emission.fraction `);
+        }
+        $R->run(q` pre = gather(pre, Type, Emission.Fraction, -Mechanisms, -Speciation, -Ox) `,
+                q` data = rbind(data, pre) `,
+        );
+    }
+}
+$R->run(q` data$Speciation = factor(data$Speciation, levels = c("TNO", "IPCC", "EMEP", "DE94", "GR95", "GR05", "UK98", "UK08")) `);
+$R->run(q` data = data %>% mutate(Mechanism = factor(Mechanisms, labels = c("MOZART-4", "RADM2")))  %>% select(-Mechanisms) `);
+$R->run(q` data = data %>% filter(Type == "Alkanes" | Type == "Aromatics" | Type == "Oxygenated") `);
+
+$R->run(q` plot.lines = function () { list( theme_tufte() ,
+                                            ylab("Total Ox Production (molecules cm-3)") ,
+                                            theme(legend.title = element_blank()) ,
+                                            theme(axis.line = element_line(colour = "black")) ,
+                                            theme(axis.title = element_text(face = "bold")) ,
+                                            theme(strip.text = element_text(face = "bold")) ,
+                                            theme(panel.margin = unit(5, "mm")) ) } `);
+
+$R->run(q` speciation.colours = c("TNO" = "#000000", "IPCC" = "#2b9eb3", "EMEP" = "#ef6638", "DE94" = "#f9c500", "GR95" = "#6c254f", "GR05" = "#0352cb", "UK98" = "#0e5c28", "UK08" = "#b569b3") `);
+
+$R->run(q` alkanes = data %>% filter(Type == "Alkanes") %>% select(-Type) `);
+$R->run(q` alkanes.eqn = alkanes %>% group_by(Mechanism) %>% do(mod2 = cor(.$Ox, .$Emission.Fraction, method = "pearson")) %>% mutate(Label = paste('"r = ', sprintf('%1.3f', mod2[1]), '"')) %>% select(-mod2) `);
+
+$R->run(q` plot = ggplot(alkanes, aes(x = Emission.Fraction, y = Ox, colour = Speciation)) `,
+        q` plot = plot + geom_point(size = 4) `,
+        q` plot = plot + facet_wrap( ~ Mechanism, scales = "free") `,
+        q` plot = plot + plot.lines() `,
+        q` plot = plot + scale_x_continuous(label = percent, limits = c(0, 0.75)) `,
+        q` plot = plot + scale_y_continuous(limits = c(1e9, 2e9)) `,
+        q` plot = plot + xlab("Percent Emissions") `,
+        q` plot = plot + scale_colour_manual(values = speciation.colours, limits = levels(data$Speciation)) `,
+        q` plot = plot + theme(legend.position = "top") `,
+        q` plot = plot + geom_text(data = alkanes.eqn, aes(x = 0.45, y = 2e9, label = Label), colour = "black", inherit.aes = FALSE, parse = TRUE) + geom_smooth(method = "lm", se = FALSE, colour = "black") `,
+        #q` plot = list(plot) `,
+);
+
+$R->run(q` aromatics = data %>% filter(Type == "Aromatics") %>% select(-Type) `);
+$R->run(q` aromatics.eqn = aromatics %>% group_by(Mechanism) %>% do(mod2 = cor(.$Ox, .$Emission.Fraction, method = "pearson")) %>% mutate(Label = paste('"r = ', sprintf('%1.3f', mod2[1]), '"')) %>% select(-mod2) `);
+
+$R->run(q` plot.1 = ggplot(aromatics, aes(x = Emission.Fraction, y = Ox, colour = Speciation)) `,
+        q` plot.1 = plot.1 + geom_point(size = 4) `,
+        q` plot.1 = plot.1 + facet_wrap( ~ Mechanism, scales = "free") `,
+        q` plot.1 = plot.1 + plot.lines() `,
+        q` plot.1 = plot.1 + scale_x_continuous(label = percent, limits = c(0, 0.75)) `,
+        q` plot.1 = plot.1 + scale_y_continuous(limits = c(1e9, 2e9)) `,
+        q` plot.1 = plot.1 + xlab("Percent Emissions") `,
+        q` plot.1 = plot.1 + scale_colour_manual(values = speciation.colours, limits = levels(data$Speciation)) `,
+        q` plot.1 = plot.1 + theme(legend.position = "top") `,
+        q` plot.1 = plot.1 + geom_text(data = aromatics.eqn, aes(x = 0.45, y = 2e9, label = Label), colour = "black", inherit.aes = FALSE, parse = TRUE) + geom_smooth(method = "lm", se = FALSE, colour = "black") `,
+        #q` plot.1 = list(plot.1) `,
+);
+
+$R->run(q` oxygenated = data %>% filter(Type == "Oxygenated") %>% select(-Type) `);
+$R->run(q` oxygenated.eqn = oxygenated %>% group_by(Mechanism) %>% do(mod2 = cor(.$Ox, .$Emission.Fraction, method = "pearson")) %>% mutate(Label = paste('"r = ', sprintf('%1.3f', mod2[1]), '"')) %>% select(-mod2) `);
+
+$R->run(q` plot.2 = ggplot(oxygenated, aes(x = Emission.Fraction, y = Ox, colour = Speciation)) `,
+        q` plot.2 = plot.2 + geom_point(size = 4) `,
+        q` plot.2 = plot.2 + facet_wrap( ~ Mechanism, scales = "free") `,
+        q` plot.2 = plot.2 + plot.lines() `,
+        q` plot.2 = plot.2 + scale_x_continuous(label = percent, limits = c(0, 0.75)) `,
+        q` plot.2 = plot.2 + scale_y_continuous(limits = c(1e9, 2e9)) `,
+        q` plot.2 = plot.2 + xlab("Percent Emissions") `,
+        q` plot.2 = plot.2 + scale_colour_manual(values = speciation.colours, limits = levels(data$Speciation)) `,
+        q` plot.2 = plot.2 + theme(legend.position = "top") `,
+        q` plot.2 = plot.2 + geom_text(data = oxygenated.eqn, aes(x = 0.45, y = 2e9, label = Label), colour = "black", inherit.aes = FALSE, parse = TRUE) + geom_smooth(method = "lm", se = FALSE, colour = "black") `,
+        #q` plot.2 = list(plot.2) `,
+);
+
+$R->run(q` plots = c(list(plot), list(plot.1), list(plot.2)) `);
+$R->run(q` CairoPDF(file = "Ox_production_vs_alkane_aromatic_oxygenated_VOC_emission_fractions_first_day.pdf", width = 10, height = 15) `);
+$R->run(q` multiplot = grid.arrange(plots[[1]] + theme(axis.title = element_blank()), 
+                                    plots[[2]] + theme(axis.title.x = element_blank(), legend.position = "none"), 
+                                    plots[[3]] + theme(legend.position = "none", axis.title.y = element_blank())) `,
+                                                #left = textGrob("Ox Production (molecules cm-3)"), gp = gpar(fontface = "bold"), rot = 90, vjust = 0.5) `,
+);
+
+$R->run(#q` CairoPDF(file = "Ox_production_vs_alkane_aromatic_oxygenated_VOC_emission_fractions_all_data.pdf", width = 10, height = 21) `,
+        q` print(multiplot) `,
+        q` dev.off() `,
+);
+
+$R->stop();
+
+sub get_no2_reservoirs { #get species that are produced when radical species react with NO2
+    my ($kpp, $file) = @_; 
+    open my $in, '<:encoding(utf-8)', $file or die $!; 
+    my @ro2;
+    for (<$in>) {
+        push @ro2, split /\s+/, $_; 
+    }
+    close $in;
+    my @no2_reservoirs;
+    foreach my $ro2 (@ro2) {
+        my ($reactions) = $kpp->reacting_with($ro2, 'NO2');
+        foreach my $reaction (@$reactions) {
+            my ($products) = $kpp->products($reaction);
+            if (@$products == 1) {
+                push @no2_reservoirs, $products->[0];
+            }   
+        }   
+    }   
+    return @no2_reservoirs;
+} 
+
+sub get_Ox_production {
+    my ($kpp, $mecca) = @_;
+    $kpp->family({
+            name    => "Ox",
+            members => $families{"Ox"},
+            weights => $weights{"Ox"},
+    });
+    my $producers = $kpp->producing("Ox");
+    my $producer_yields = $kpp->effect_on("Ox", $producers);
+    print "No producers\n" if (@$producers == 0);
+    print "Ox\n";
+
+    my $total_Ox = 0;
+    for (0..$#$producers) {
+        my $reaction = $producers->[$_];
+        my $reaction_number = $kpp->reaction_number($reaction);
+        my $rate = $mecca->rate($reaction_number) * $producer_yields->[$_];
+        next if ($rate->sum == 0);
+        $rate = $rate->reshape($n_per_day, $n_days);
+        $rate = $rate->sumover;
+        $total_Ox += $rate->at(0);
+    }
+    return $total_Ox;
+}
+
+sub get_emission_fractions {
+    my ($kpp, $mecca, $mechanism, $speciation) = @_;
+    my %emissions;
+    my @alkanes = qw( C2H6 C3H8 NC4H10 IC4H10 NC5H12 IC5H12 NEOP NC6H14 M2PE M3PE NC7H16 M2HEX M3HEX NC8H18 NC9H20 NC10H22 NC11H24 NC12H26 BIGALK ETH HC3 HC5 HC8 CHEX );
+    my @alkenes = qw( C2H4 C3H6 BUT1ENE LIMONENE APINENE BPINENE BIGENE OL2 OLT OLI C10H16 C2H2 ISOP ISO C5H8 );
+    my @aromatics = qw( BENZENE TOLUENE MXYL OXYL PXYL EBENZ PBENZ IPBENZ TM123B TM124B TM135B STYRENE METHTOL OETHTOL PETHTOL DIME35EB TOL XYL );
+    my @chlorinated = qw( CH2CL2 CH3CCL3 TCE TRICLETH );
+    my @oxygenated = qw( ETHACET NBUTACET IPROACET NPROACET CH3COCH3 MEK MIBK CYHEXONE CH3OH C2H5OH IPROPOL NBUTOL NPROPOL BUT2OL IBUTOL MIBKAOH C6H5CH2OH ETHGLY PROPGLY BUOX2ETOH PR2OHMOX EOX2EOL CH3OCH3 MO2EOL HCOOH CH3CO2H PROPACID HCHO CH3CHO C2H5CHO C3H7CHO IPRCHO C4H9CHO ACR MACR C4ALDB CH2O HCHO ALD KET CH3COOH ORA1 ORA2 );
+
+    print "Emissions: $mechanism  : $speciation\n";
+    my $emitting_reactions = $kpp->consuming("UNITY");
+    for (0..$#$emitting_reactions) {
+        my $reaction = $emitting_reactions->[$_];
+        my $reaction_number = $kpp->reaction_number($reaction);
+        my $rate = $mecca->rate($reaction_number);
+        next if ($rate->sum == 0);
+        my $VOC = $kpp->products($reaction)->[0];
+        next if ($VOC eq "NO"); 
+        if ($VOC ~~ @alkanes) {
+            #print "$VOC : alkane\n";
+            $emissions{"Alkanes"} += $rate->sum;
+        } elsif ($VOC ~~ @alkenes) {
+            #print "$VOC : alkene or alkyne\n";
+            $emissions{"Alkenes, Alkynes"} += $rate->sum;
+        } elsif ($VOC ~~ @aromatics) {
+            #print "$VOC : aromatic\n";
+            $emissions{"Aromatics"} += $rate->sum;
+        } elsif ($VOC ~~ @chlorinated) {
+            #print "$VOC : chlorinated\n";
+            $emissions{"Chlorinated"} += $rate->sum;
+        } elsif ($VOC ~~ @oxygenated) {
+            #print "$VOC : oxygenated\n";
+            $emissions{"Oxygenated"} += $rate->sum;
+        } else {
+            print "No match for $VOC\n";
+        }
+    }
+
+    if ($mechanism eq "RADM2") {
+        my $contribution_file = "Fractional_Contributions_of_individual_RADM2_species_to_functional_groups.csv";
+        open my $in, '<:encoding(utf-8)', $contribution_file or die $!;
+        my @lines = <$in>;
+        close $in;
+        my %RADM2;
+        foreach my $line (@lines) {
+            next if ($line =~ /^Speci/);
+            chomp $line;
+            my ($speciation, $group, $contribution) = split /,/, $line;
+            $RADM2{$speciation}{$group} = $contribution;
+        }
+
+        $emissions{"Chlorinated"} += $RADM2{$speciation}{"Chlorinated"} * $emissions{"Alkanes"};
+        $emissions{"Oxygenated"} += $RADM2{$speciation}{"Oxygenated"} * $emissions{"Alkanes"};
+        $emissions{"Alkenes, Alkynes"} += $RADM2{$speciation}{"Alkenes;Alkynes"} * $emissions{"Alkanes"};
+        $emissions{"Alkanes"} += $RADM2{$speciation}{"Alkanes"} * $emissions{"Alkanes"};
+    } elsif ($mechanism eq "MOZART") {
+        my $contribution_file = "Fractional_Contributions_of_individual_MOZART_species_to_functional_groups.csv";
+        open my $in, '<:encoding(utf-8)', $contribution_file or die $!;
+        my @lines = <$in>;
+        close $in;
+        my %MOZART;
+        foreach my $line (@lines) {
+            next if ($line =~ /^Speci/);
+            chomp $line;
+            my ($speciation, $group, $contribution) = split /,/, $line;
+            $MOZART{$speciation}{$group} = $contribution;
+        }
+
+        $emissions{"Chlorinated"} += $MOZART{$speciation}{"Chlorinated"} * $emissions{"Alkanes"};
+        $emissions{"Oxygenated"} += $MOZART{$speciation}{"Oxygenated"} * $emissions{"Alkanes"};
+        $emissions{"Alkanes"} += $MOZART{$speciation}{"Alkanes"} * $emissions{"Alkanes"};
+    }
+    my $total_emissions = 0;
+    $total_emissions += $emissions{$_} foreach (keys %emissions);
+    $emissions{$_} /= $total_emissions foreach (keys %emissions);
+    return \%emissions;
+}
---
+++ Mixing_ratios/bio_anthro_mixing_ratio_differences_statistics.pl
@@ -0,0 +1,167 @@
+#! /usr/bin/env perl
+# statistics of ozone mixing ratio differences in each mechanism between the speciations
+# Version 0: Jane Coates 22/7/2015
+
+use strict;
+use diagnostics;
+use MECCA;
+use PDL;
+use PDL::NiceSlice;
+use Statistics::R;
+
+my $species = "O3";
+
+my $base = "/work/users/jco/Solvent_Emissions";
+my @mechanisms = qw(MCM MOZART RADM2);
+my @speciations = qw( DE94 EMEP GR05 GR95 IPCC TNO UK08 UK98 );
+my @runs = qw( bio_and_anthro );
+my %data;
+
+my $mecca = MECCA->new("$base/RADM2/DE94_bio_and_anthro/boxmodel");
+my $ntime = $mecca->time->nelem;
+my $dt = $mecca->dt->at(0);
+my $n_per_day = 86400 / $dt;
+my $n_days = int $ntime / $n_per_day;
+my $times = $mecca->time;
+$times = $times(1:$ntime-2);
+
+foreach my $mechanism (@mechanisms) {
+    foreach my $speciation (@speciations) {
+        foreach my $run (@runs) {
+            my $dir = "$base/$mechanism/${speciation}_$run";
+            my $mecca = MECCA->new("$dir/boxmodel");
+            my $mr = $mecca->tracer($species); 
+            $data{$mechanism}{$speciation} = $mr(1:$ntime-2);
+        }
+    }
+}
+
+my $R = Statistics::R->new();
+$R->run(q` library(tidyr) `,
+        q` library(dplyr) `,
+        q` library(ggplot2) `,
+        q` library(Cairo) `,
+        q` library(ggthemes) `,
+);
+
+$R->set('Time', [ map { $_ } $times->dog ]);
+$R->run(q` d = data.frame() `);
+    foreach my $mechanism (sort keys %data) {
+        $R->set('mechanism', $mechanism);
+        $R->run(q` pre = data.frame(Mechanism = rep(mechanism, length(Time))) `);
+        foreach my $speciation (sort keys %{$data{$mechanism}}) {
+            $R->set('speciation', $speciation);
+            $R->set('mixing.ratio', [  map { $_ } $data{$mechanism}{$speciation}->dog ]);
+            $R->run(q` pre[speciation] = mixing.ratio `);
+        }
+#        $R->run(q` pre = gather(pre, Speciation, Mixing.Ratio, -Time, -Run, -Mechanism) `,
+        $R->run(q` d = rbind(d, pre) `);
+#        );
+}
+#$R->run(q` write.table(d, file = "ozone_mixing_ratios.csv", quote = FALSE, row.names = FALSE, sep = ",") `);
+#By Mechanism analysis
+$R->run(q` radm2 = d %>% filter(Mechanism == "RADM2") %>% select(-Mechanism) `,
+        q` mozart = d %>% filter(Mechanism == "MOZART") %>% select(-Mechanism) `,
+        q` mcm = d %>% filter(Mechanism == "MCM") %>% select(-Mechanism) `,
+);
+
+$R->run(q` radm2 = radm2 %>% rowwise() %>% mutate(Min = min(DE94, EMEP, GR05, GR95, IPCC, TNO, UK08, UK98)) %>% mutate(Max = max(DE94, EMEP, GR05, GR95, IPCC, TNO, UK08, UK98)) `,
+        q` radm2 = radm2 %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `, 
+        q` radm2.sd = sd(radm2$Largest.Diff) `,
+        q` radm2.mean = mean(radm2$Largest.Diff) `,
+);
+
+$R->run(q` mozart = mozart %>% rowwise() %>% mutate(Min = min(DE94, EMEP, GR05, GR95, IPCC, TNO, UK08, UK98)) %>% mutate(Max = max(DE94, EMEP, GR05, GR95, IPCC, TNO, UK08, UK98)) `,
+        q` mozart = mozart %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `, 
+        q` mozart.sd = sd(mozart$Largest.Diff) `,
+        q` mozart.mean = mean(mozart$Largest.Diff) `,
+);
+
+$R->run(q` mcm = mcm %>% rowwise() %>% mutate(Min = min(DE94, EMEP, GR05, GR95, IPCC, TNO, UK08, UK98)) %>% mutate(Max = max(DE94, EMEP, GR05, GR95, IPCC, TNO, UK08, UK98)) `,
+        q` mcm = mcm %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `, 
+        q` mcm.sd = sd(mcm$Largest.Diff) `,
+        q` mcm.mean = mean(mcm$Largest.Diff) `,
+);
+#my $p = $R->run(q` print(radm2.day2.mean) `);
+#print $p, "\n";
+#$p = $R->run(q` print(radm2.day3.mean) `);
+#print $p, "\n";
+
+$R->run(q` entire.data = data.frame(Mechanism = "MCM", St.Dev = mcm.sd, Average = mcm.mean) `,
+        q` entire.data = rbind(entire.data, data.frame(Mechanism = 'MOZART', St.Dev = mozart.sd, Average = mozart.mean)) `,
+        q` entire.data = rbind(entire.data, data.frame(Mechanism = 'RADM2', St.Dev = radm2.sd, Average = radm2.mean)) `,
+        q` entire.data$St.Dev = sprintf("%.3e", entire.data$St.Dev) `,
+        q` entire.data$Average = sprintf("%.3e", entire.data$Average) `,
+);
+$R->run(q` write.table(entire.data, file = "bio_and_anthro_Ozone_time_series_entire_difference_statistics_by_mechanism.txt", row.names = FALSE, quote = FALSE, sep = "\t") `);
+
+#By Speciation analysis
+$R->run(q` d$Time = Time `,
+        q` d = d %>% gather(Speciation, Mixing.Ratio, -Mechanism, -Time) `,
+        q` d = spread(d, Mechanism, Mixing.Ratio) `,
+
+        q` de94 = d %>% filter(Speciation == "DE94") %>% select(-Speciation, -Time) `,
+        q` de94 = de94 %>% rowwise() %>% mutate(Min = min(MCM, MOZART, RADM2)) %>% mutate(Max = max(MCM, MOZART, RADM2)) `,
+        q` de94 = de94 %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `,
+        q` de94.entire.sd = sd(de94$Largest.Diff) `,
+        q` de94.entire.mean = mean(de94$Largest.Diff) `,
+
+        q` emep = d %>% filter(Speciation == "EMEP") %>% select(-Speciation, -Time) `,
+        q` emep = emep %>% rowwise() %>% mutate(Min = min(MCM, MOZART, RADM2)) %>% mutate(Max = max(MCM, MOZART, RADM2)) `,
+        q` emep = emep %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `,
+        q` emep.entire.sd = sd(emep$Largest.Diff) `,
+        q` emep.entire.mean = mean(emep$Largest.Diff) `,
+
+        q` gr05 = d %>% filter(Speciation == "GR05") %>% select(-Speciation, -Time) `,
+        q` gr05 = gr05 %>% rowwise() %>% mutate(Min = min(MCM, MOZART, RADM2)) %>% mutate(Max = max(MCM, MOZART, RADM2)) `,
+        q` gr05 = gr05 %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `,
+        q` gr05.entire.sd = sd(gr05$Largest.Diff) `,
+        q` gr05.entire.mean = mean(gr05$Largest.Diff) `,
+
+        q` gr95 = d %>% filter(Speciation == "GR95") %>% select(-Speciation, -Time) `,
+        q` gr95 = gr95 %>% rowwise() %>% mutate(Min = min(MCM, MOZART, RADM2)) %>% mutate(Max = max(MCM, MOZART, RADM2)) `,
+        q` gr95 = gr95 %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `,
+        q` gr95.entire.sd = sd(gr95$Largest.Diff) `,
+        q` gr95.entire.mean = mean(gr95$Largest.Diff) `,
+
+        q` ipcc = d %>% filter(Speciation == "IPCC") %>% select(-Speciation, -Time) `,
+        q` ipcc = ipcc %>% rowwise() %>% mutate(Min = min(MCM, MOZART, RADM2)) %>% mutate(Max = max(MCM, MOZART, RADM2)) `,
+        q` ipcc = ipcc %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `,
+        q` ipcc.entire.sd = sd(ipcc$Largest.Diff) `,
+        q` ipcc.entire.mean = mean(ipcc$Largest.Diff) `,
+
+        q` tno = d %>% filter(Speciation == "TNO") %>% select(-Speciation, -Time) `,
+        q` tno = tno %>% rowwise() %>% mutate(Min = min(MCM, MOZART, RADM2)) %>% mutate(Max = max(MCM, MOZART, RADM2)) `,
+        q` tno = tno %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `,
+        q` tno.entire.sd = sd(tno$Largest.Diff) `,
+        q` tno.entire.mean = mean(tno$Largest.Diff) `,
+
+        q` uk98 = d %>% filter(Speciation == "UK98") %>% select(-Speciation, -Time) `,
+        q` uk98 = uk98 %>% rowwise() %>% mutate(Min = min(MCM, MOZART, RADM2)) %>% mutate(Max = max(MCM, MOZART, RADM2)) `,
+        q` uk98 = uk98 %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `,
+        q` uk98.entire.sd = sd(uk98$Largest.Diff) `,
+        q` uk98.entire.mean = mean(uk98$Largest.Diff) `,
+
+        q` uk08 = d %>% filter(Speciation == "UK08") %>% select(-Speciation, -Time) `,
+        q` uk08 = uk08 %>% rowwise() %>% mutate(Min = min(MCM, MOZART, RADM2)) %>% mutate(Max = max(MCM, MOZART, RADM2)) `,
+        q` uk08 = uk08 %>% rowwise() %>% mutate(Largest.Diff = Max - Min) `,
+        q` uk08.entire.sd = sd(uk08$Largest.Diff) `,
+        q` uk08.entire.mean = mean(uk08$Largest.Diff) `,
+);
+#my $p = $R->run(q` print(length(de94$Max)) `);
+#print $p, "\n";
+
+$R->run(q` entire.data = data.frame(Speciation = "DE94", St.Dev = de94.entire.sd, Average = de94.entire.mean) `,
+        q` entire.data = rbind(entire.data, data.frame(Speciation = 'EMEP', St.Dev = emep.entire.sd, Average = emep.entire.mean)) `,
+        q` entire.data = rbind(entire.data, data.frame(Speciation = 'GR05', St.Dev = gr05.entire.sd, Average = gr05.entire.mean)) `,
+        q` entire.data = rbind(entire.data, data.frame(Speciation = 'GR95', St.Dev = gr95.entire.sd, Average = gr95.entire.mean)) `,
+        q` entire.data = rbind(entire.data, data.frame(Speciation = 'IPCC', St.Dev = ipcc.entire.sd, Average = ipcc.entire.mean)) `,
+        q` entire.data = rbind(entire.data, data.frame(Speciation = 'TNO', St.Dev = tno.entire.sd, Average = tno.entire.mean)) `,
+        q` entire.data = rbind(entire.data, data.frame(Speciation = 'UK08', St.Dev = uk08.entire.sd, Average = uk08.entire.mean)) `,
+        q` entire.data = rbind(entire.data, data.frame(Speciation = 'UK98', St.Dev = uk98.entire.sd, Average = uk98.entire.mean)) `,
+        q` entire.data$St.Dev = sprintf("%.3e", entire.data$St.Dev) `,
+        q` entire.data$Average = sprintf("%.3e", entire.data$Average) `,
+);
+$R->run(q` write.table(entire.data, file = "bio_and_anthro_Ozone_time_series_entire_difference_statistics_by_speciation.txt", row.names = FALSE, quote = FALSE, sep = "\t") `);
+
+$R->stop();
